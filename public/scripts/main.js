3301eb03040e6843665fbb07010b76c6d9feb4cc
Daniel Miolan <miolan96@gmail.com>
2023-07-17 07:51:12 -0400

    Create async function to request json data.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,23 @@
+'use strict'
+
+let data = null;
+
+async function GetJsonData() {
+    const url = `${window.location.href}scripts/data.json`;
+
+    try {
+      const response = await fetch(url);
+      data = await response.json();
+      console.log(data); 
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+
 function Init() {
-    console.log('Script Init');
+    console.log('Script Init')
+    GetJsonData()
 }
 
 Init();
