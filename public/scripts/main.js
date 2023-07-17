b3412c1676936752d98b850bdde36cd07c243a3c
Daniel Miolan <miolan96@gmail.com>
2023-07-17 08:45:50 -0400

    Create group by week logic.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,11 +7,12 @@
 let data = null 
 //Group By selected where:
 // 0 = week, 1 = location, 2 = author
-let groupType = 2 
+let groupType = 0
 // Will hold grouped JSON data
 let dataGrouped = [] 
 // Container where dynamic content will be display
 let dynamicContent = document.getElementById("dynamic") 
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"]
 
 
 // ===========================
@@ -24,6 +25,8 @@ async function GetJsonData() {
       const response = await fetch(url)
       data = await response.json()
       console.log(data)
+      
+      // Perform group by when data is ready
       PerformGroup()
     } catch (error) {
       console.error('Error:', error)
@@ -48,22 +51,39 @@ function PerformGroup() {
 }
 
 function GroupByWeek() {
-    // TO-DO
+    // Iterate trougth json data
+    for (const post of data) {
+        // Store the date for easy usage
+        // by using the GetDate utility function
+        const date = GetDate(post.time)
+        // We create a key name using the week #
+        // and the date year
+        const key = `Week ${date.week} - ${date.year}`
+        
+        // If no key found in our dictionary the key is created
+        if (!dataGrouped[key]) {
+            dataGrouped[key] = []
+        }
+        
+        // Store the post data in the dictionary
+        dataGrouped[key].push(post)
+    }
+
+    console.log(dataGrouped)
 }
 
 function GroupByLocation() {
     // Iterate trougth json data
     for (const post of data) {
-        // We store the location for easy usage
+        // Store the location for easy usage
         const location = post.location
 
-        // If no key on our dictionary is found
-        // we create the key
+        // If no key found in our dictionary the key is created
         if (!dataGrouped[location]) {
             dataGrouped[location] = []
         }
 
-        // We store the post data in the dictionary
+        // Store the post data in the dictionary
         dataGrouped[location].push(post)
     }
 
@@ -73,20 +93,54 @@ function GroupByLocation() {
 function GroupByAuthor() {
     // Iterate trougth json data
     for (const post of data) {
-         // We store the author for easy usage
+         // Store the author for easy usage
         const author = post.author
         
-        // If no key on our dictionary is found
-        // we create the key
+        // If no key found in our dictionary the key is created
         if (!dataGrouped[author]) {
             dataGrouped[author] = []
         }
 
-        // We store the post data in the dictionary
+        // Store the post data in the dictionary
         dataGrouped[author].push(post)
     }
 
     console.log(dataGrouped)
+}
+
+// ===========================
+// UTILITY FUNCTIONS
+// ===========================
+function GetDate(value) {
+    // Create a date object using the time
+    // in milliseconds from JSON
+    const date = new Date(value)
+
+    // Return an object with all the important
+    // values from the date 
+    return {
+        year: date.getFullYear(),
+        month: date.getMonth(),
+        monthStr: months[date.getMonth()], // Month in string
+        day: date.getDate(),
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds(),
+        week: GetWeekNumber(date) // Get week number of date year
+    }
+}
+
+function GetWeekNumber(date) {
+    // Store the milliseconds a day have
+    const dayMilliseconds = 86400000
+    // Create a date for the first day of the given date year
+    const janFirst = new Date(date.getFullYear(), 0, 1)
+    // Get the difference to know the time passed in that year
+    const  timeDif = date - janFirst
+    
+    // Calculate how many days pass in given year and divided by 7
+    // to get the week number
+    return Math.ceil((timeDif / dayMilliseconds + janFirst.getDay() + 1) / 7)
 }
 
 // ===========================
